60d3975c35445f22e117fcd6fbafcca14fe9905a
Verbalena <alen.verb@yandex.ru>
2024-08-07 14:53:34 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,47 @@
+class Animal:            # БАЗОВЫЙ класс Животные
+    alive = True         # атрибут класса (живой)
+    fed = False          # атрибут класса (накормленный)
+
+    def __init__(self, name):
+        self.name = name
+    def eat(self, food):
+        Plant.name = food
+        if food.edible == True:
+            print(f'{self.name} съел {food.name}')
+            Animal.fed = True
+        else:
+            if food.edible == False:
+                print(f'{self.name} не стал есть {food.name}')
+                Animal.alive = False
+class Mammal(Animal):    # дочерний класс
+    pass
+class Predator(Animal):  # дочерний класс
+    pass
+
+class Plant:           # БАЗОВЫЙ класс Растения
+    edible = False     # атрибут класса (съедобнность)
+    def __init__(self, name):
+        self.name = name
+
+class Flower(Plant):   # дочерний класс Цветы
+    pass
+class Fruit(Plant):    # дочерний класс Фрукты
+    edible = True   # атрибут дочернего класса (переопределён)
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
